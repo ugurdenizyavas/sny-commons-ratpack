971660a10f83fa255473ffeb7b5fba512ebe37ac
trbasarb <baris.basaran@eu.sony.com>
2014-11-20 16:39:09 +0200

    unified delta service responses

--- src/main/groovy/com/sony/ebs/octopus3/commons/ratpack/handlers/HazelcastAwareDeltaHandler.groovy
+++ src/main/groovy/com/sony/ebs/octopus3/commons/ratpack/handlers/HazelcastAwareDeltaHandler.groovy
@@ -1,20 +1,22 @@
 package com.sony.ebs.octopus3.commons.ratpack.handlers
 
 import com.hazelcast.core.HazelcastInstance
+import com.sony.ebs.octopus3.commons.flows.Delta
 import com.sony.ebs.octopus3.commons.flows.FlowTypeEnum
 import com.sony.ebs.octopus3.commons.flows.ServiceTypeEnum
 import com.sony.ebs.octopus3.commons.process.ProcessIdImpl
 import com.sony.ebs.octopus3.commons.ratpack.file.ResponseStorage
+import com.sony.ebs.octopus3.commons.ratpack.product.cadc.delta.model.DeltaResult
+import com.sony.ebs.octopus3.commons.ratpack.product.cadc.delta.service.DeltaResultService
 import groovy.json.JsonOutput
 import groovy.util.logging.Slf4j
+import org.joda.time.DateTime
 import org.springframework.beans.factory.annotation.Autowired
 import ratpack.groovy.handling.GroovyContext
 import ratpack.groovy.handling.GroovyHandler
-import com.sony.ebs.octopus3.commons.flows.Delta
+import ratpack.jackson.JsonRender
 
 import java.util.concurrent.CopyOnWriteArrayList
-
-import static ratpack.jackson.Jackson.json
 
 /**
  * author: TRYavasU
@@ -31,6 +33,9 @@ abstract class HazelcastAwareDeltaHandler<D extends Delta> extends GroovyHandler
 
     @Autowired
     ResponseStorage responseStorage
+
+    @Autowired
+    DeltaResultService deltaResultService
 
     void finalizeInAsyncThread(delta) {
         ongoingProcesses.remove delta.ticket
@@ -54,13 +59,13 @@ abstract class HazelcastAwareDeltaHandler<D extends Delta> extends GroovyHandler
             if (delta.ticket in ongoingProcesses) {
                 log.warn "Duplicate request ignored for delta {}", delta
                 activity.warn "{} request is ignored because there's already an ongoing delta for {}-{}", delta, delta.publication, delta.locale
-                context.response.status 400
                 delta.status = 400
                 errors << ["Duplicate request ignored for delta"]
-                def jsonResponse = json(status: 400, errors: errors, delta: delta)
 
-                responseStorage.store(delta.processId.id, [getFlow().toString().toLowerCase(), "delta", delta.publication, delta.locale, delta.processId.id], JsonOutput.toJson(jsonResponse.object))
+                def jsonResponse = deltaResultService.createDeltaResultInvalid(delta, errors)
+                storeResponse(delta, jsonResponse)
 
+                context.response.status 400
                 context.render jsonResponse
             } else {
                 ongoingProcesses << delta.ticket
@@ -69,15 +74,44 @@ abstract class HazelcastAwareDeltaHandler<D extends Delta> extends GroovyHandler
         } else {
             log.warn "Validation fails for {}", delta
             activity.error "{} request is rejected because validation fails", delta
-            context.response.status 400
             delta.status = 400
-            def jsonResponse = json(status: 400, errors: errors, delta: delta)
 
+            def jsonResponse = deltaResultService.createDeltaResultInvalid(delta, errors)
+            storeResponse(delta, jsonResponse)
+
+            context.response.status 400
             context.render jsonResponse
-
-            responseStorage.store(delta.processId.id, [getFlow().toString().toLowerCase(), "delta", delta.publication, delta.locale, delta.processId.id], JsonOutput.toJson(jsonResponse.object))
         }
 
+    }
+
+    void storeResponse(Delta delta, JsonRender jsonRender) {
+        def urnValues = [delta.flow?.toString()?.toLowerCase(), delta.service?.toString()?.toLowerCase(), delta.publication, delta.locale, delta.processId?.id]
+        responseStorage.store(delta.processId?.id, urnValues, JsonOutput.toJson(jsonRender.object))
+    }
+
+    JsonRender processError(Delta delta, List<String> errors, DateTime startTime) {
+        finalizeInAsyncThread(delta)
+
+        activity.error "finished {} with errors: {}", delta, errors
+        def endTime = new DateTime()
+        delta.status = 500
+
+        def jsonResponse = deltaResultService.createDeltaResultWithErrors(delta, errors, startTime, endTime)
+        storeResponse(delta, jsonResponse)
+        jsonResponse
+    }
+
+    JsonRender processSuccess(Delta delta, DeltaResult deltaResult, DateTime startTime) {
+        finalizeInAsyncThread(delta)
+
+        activity.info "finished {} with success", delta
+        def endTime = new DateTime()
+        delta.status = 200
+
+        def jsonResponse = deltaResultService.createDeltaResult(delta, deltaResult, startTime, endTime)
+        storeResponse(delta, jsonResponse)
+        jsonResponse
     }
 
     /**
