32d3b15b9dc3e3b01b0a5a4bddd19f4fa53dc400
tryavasu <ugurdeniz.yavas@eu.sony.com>
2014-11-25 10:03:48 +0200

    Do not write response if delta.upload is false

--- src/main/groovy/com/sony/ebs/octopus3/commons/ratpack/file/ResponseStorage.groovy
+++ src/main/groovy/com/sony/ebs/octopus3/commons/ratpack/file/ResponseStorage.groovy
@@ -1,5 +1,6 @@
 package com.sony.ebs.octopus3.commons.ratpack.file
 
+import com.sony.ebs.octopus3.commons.flows.Delta
 import com.sony.ebs.octopus3.commons.urn.URNCreationException
 import com.sony.ebs.octopus3.commons.ratpack.http.Oct3HttpClient
 import com.sony.ebs.octopus3.commons.urn.URNImpl
@@ -20,29 +21,31 @@ class ResponseStorage {
     String saveUrl
 
     /**
-     * Stores given response into repository to path [/responses/{urnValues}/{processId]]
+     * Stores given response into repository to path [/responses/{urnValues}/{processId]] if delta.upload is true
      *
-     * @param processId processId of the current job
-     * @param urnValues values of the urn to copy response (urn type is responses)
+     * @param delta Delta object of the flow
      * @param response response to save
      * @return true if response is saved successfully
      */
-    boolean store(String processId, ArrayList<String> urnValues, String response) {
-        try {
-            def url = new URIBuilder(saveUrl.replace(":urn", new URNImpl("responses", urnValues).toString())).addQueryParam("processId", processId).toString()
+    boolean store(Delta delta, String response) {
+        if (delta.upload) {
+            def urnValues = [delta.flow?.toString()?.toLowerCase(), delta.service?.toString()?.toLowerCase(), delta.publication, delta.locale, delta.processId?.id]
+            try {
+                def url = new URIBuilder(saveUrl.replace(":urn", new URNImpl("responses", urnValues).toString())).addQueryParam("processId", delta.processId.id).toString()
 
-            httpClient.doPost(url, IOUtils.toInputStream(response, Charset.forName('UTF-8'))).subscribe(
-                    {
-                        log.debug "Response is sent to repository with url: {}", url
-                        it.success
-                    },
-                    { e ->
-                        log.error "Error in sending response to repository with url: ${url}", e
-                        false
-                    }
-            )
-        } catch (URNCreationException e) {
-            log.warn "Cannot write response to repository since urn parameters are invalid", e
+                httpClient.doPost(url, IOUtils.toInputStream(response, Charset.forName('UTF-8'))).subscribe(
+                        {
+                            log.debug "Response is sent to repository with url: {}", url
+                            it.success
+                        },
+                        { e ->
+                            log.error "Error in sending response to repository with url: ${url}", e
+                            false
+                        }
+                )
+            } catch (URNCreationException e) {
+                log.warn "Cannot write response to repository since urn parameters are invalid", e
+            }
         }
     }
 }
